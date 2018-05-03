84d536c38c6c885c0080aef2dee3382797e46916
James Henley <james.l.henley@gmail.com>
2018-05-03 19:59:18 -0400

    Adding code to use firebase authentication

--- client/src/components/NavbarRegister.jsx
+++ client/src/components/NavbarRegister.jsx
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink, Fa, Container, Mask, View, Row, Col, Card, CardBody, CardImage, CardText, CardTitle, Button, Input, Jumbotron, } from 'mdbreact';
-import { Link } from 'react-router-dom';
+import { Link, 
+    withRouter, 
+} from 'react-router-dom';
+import { auth } from '../firebase';
+import * as homeRoute from '../pages/appHome';
 //import './Navbar.css'
+
+const INITIAL_STATE = {
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    error: null,
+};
+
+const byPropKey = (propertyName, value) => () => ({ [propertyName]: value, })
+
+const RegisterNavbar = withRouter(({history}) =>
+<div>
+    <NavbarRegister history={history} />
+</div>
+)
 
 class NavbarRegister extends React.Component {
 
@@ -10,6 +29,7 @@ class NavbarRegister extends React.Component {
         this.state = {
             collapse: false,
             isWideEnough: false,
+            INITIAL_STATE
         };
         this.onClick = this.onClick.bind(this);
     }
@@ -19,8 +39,42 @@ class NavbarRegister extends React.Component {
             collapse: !this.state.collapse,
         });
     }
+
+    onSubmit = (event) => {
+        const {
+            email,
+            passwordOne,
+        } = this.state;
+
+        const {
+            history,
+        } = this.props;
+
+        auth.createUserWithEmailAndPassword(email, passwordOne)
+        .then(authUser => {
+            console.log(INITIAL_STATE);
+            this.setState(() => ({INITIAL_STATE}));
+            history.push(homeRoute.appHome);
+        })
+        .catch(error => {
+            this.setState(byPropKey('error', error));
+        });
+
+        event.preventDefault();
+    }
+
+
     render() {
         const view = { background: 'url("https://images.unsplash.com/photo-1507105306461-47f75f2da3aa?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c3a9226dabffa306b261ea52c55cc954&auto=format&fit=crop&w=1950&q=80")no-repeat center center', backgroundSize: 'cover', height: '100vh', marginTop: '-56px' }
+        
+        const {
+            email,
+            passwordOne,
+            passwordTwo,
+            error,
+        } = this.state;
+
+        const isInvalid = passwordOne !== passwordTwo || passwordOne === '' || email === '';
         return (
             <div>
                 <header>
@@ -64,7 +118,7 @@ class NavbarRegister extends React.Component {
 
                     <Container>
                         <h2 className="mb-5"> Personal Information</h2>
-                        <form>
+                        <form onSubmit={this.onSubmit}>
                             <div className="row">
 
                                 <div className="col-md-6">
@@ -90,12 +144,16 @@ class NavbarRegister extends React.Component {
                             <div className="row">
 
                                 <div className="col-md-6">
-                                    <Input label="Type your email" icon="envelope" group type="email" validate error="wrong" success="right" />
+                                    <Input label="Type your email" icon="envelope" group type="email" validate error="wrong" success="right" value={email} onChange={event => this.setState(byPropKey('email', event.target.value))}/>
                                 </div>
 
 
                                 <div className="col-md-6">
-                                <Input label="Type your password" icon="lock" group type="password" validate/>
+                                    <Input label="Type your password" icon="lock" group type="password" validate value={passwordOne} onChange={event => this.setState(byPropKey('passwordOne', event.target.value))}/>
+                                </div>
+
+                                <div className="col-md-6">
+                                    <Input label="Confirm your password" icon="lock" group type="password" validate value={passwordTwo} onChange={event => this.setState(byPropKey('passwordTwo', event.target.value))}/>
                                 </div>
                             </div>
                             <div className="row">
@@ -131,12 +189,11 @@ class NavbarRegister extends React.Component {
                             </div>
                             <div className="row mt-4">
                                 <div className="col-md-4">
-                                <Link to="/appHome" className="submitBtn"><Button color="danger" size="lg">Submit</Button></Link>
+                                    {/* <Link to="/appHome" className="submitBtn"> */}<Button color="danger" size="lg" type="submit" disabled={isInvalid}>Submit</Button>{/* </Link> */}
+                                    {error && <p>{error.message}</p>}
                                 </div>
                             </div>
                         </form>
-
-
                     </Container>
                 </header>
             </div>
@@ -144,4 +201,4 @@ class NavbarRegister extends React.Component {
     }
 }
 
-export default NavbarRegister;
+export default withRouter(RegisterNavbar);
