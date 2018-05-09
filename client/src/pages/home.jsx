ecc75e0c041d33cc0db29b34fb381613e591c612
Brandon Matthews <brandonmatthews1982@gmail.com>
2018-05-08 20:58:13 -0400

    added About.jsx to home page

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import NavbarWithIntro from '../components/NavbarWithIntro';
 import Footer from '../components/Footer';
 import Team from '../components/Team';
+import About from '../components/About';
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <NavbarWithIntro />
+                <About />
                 <Team />
                 <Footer />                
             </div>
